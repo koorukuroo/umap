2f6216490c32ea3673728871978e915c4844fbff
KimKyungHoon <KyunghoonKim@KimKyungHoonui-MacBook-Pro.local>
2016-01-20 23:49:30 +0900

    init

---
+++ setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf8 -*-
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'packages': ['umap'],
+    'description': 'UNIST synthesis MAP solution',
+    'author': 'Kyunghoon Kim',
+    'url': 'http://amath.unist.ac.kr',
+    'author_email': 'preware@gmail.com',
+    'version': '0.1',
+    'install_requirements': ['folium', 'vincenty'],
+    'scripts': [],
+    'name': 'umap',
+    'include_pakcage_data': True,
+    'package_data': {'': ['assets/*.txt']}
+}
+
+setup(**config)
---
+++ tests/umap_tests.py
@@ -0,0 +1,13 @@
+# -*- coding: utf8 -*-
+
+from nose.tools import *
+import NAME
+
+def setup():
+    print "SETUP!"
+
+def testdown():
+    print "TEAR DOWN!"
+
+def test_basic():
+    print "실행!"
